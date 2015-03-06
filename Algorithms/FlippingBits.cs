4317bb7d92b5d6ac1d7ae40a5feb08279ce3d6c1
Ayzhin <JohnAyzhin@gmail.com>
2015-03-06 08:09:55 -0600

    2 Algorithms, 1 AI, 1 Euler

---
+++ Algorithms/FlippingBits.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class FlippingBits
+    {
+        static void main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            List<UInt32> ints = new List<uint>();
+
+            for(int i = 0; i < x; i++)
+            {
+                ints.Add(Convert.ToUInt32(Console.ReadLine()));
+            }
+
+            foreach(UInt32 val in ints)
+            {
+                Console.WriteLine(~val);
+            }
+        }
+    }
+}
---
+++ Euler/Multiples3And5.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Euler
+{
+    class Multiples3And5
+    {
+        static void Main(string[] args)
+        {
+            long x = Convert.ToInt64(Console.ReadLine());
+
+            if(x >= 1 && x <= 100000)
+            {
+                for (int i = 0; i < x; i++)
+                {
+                    long w = Convert.ToInt64(Console.ReadLine());
+
+                    if(w >= 1 && w <= 1000000000)
+                    {
+                        long total = 0;
+
+                        total = func(3, w) + func(5, w) - func(15, w);
+
+                        Console.WriteLine(total);
+                    }
+                }
+            }
+        }
+
+        static long func(long a, long b)
+        {
+            long c;
+
+            b = b - 1;
+            c = b / a;
+
+            return (a * c * (c + 1)) / 2;
+        }
+    }
+}
